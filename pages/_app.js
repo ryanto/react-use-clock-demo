8659a9e8e3d10c2d3492b74f0a9351852eb20c74
Ryan Toronto <ryanto@gmail.com>
2021-05-08 22:34:17 -0400

    starting point

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import { useEffect, useReducer } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  let [state, rerender] = useReducer((s) => s + 1, 0);
+
+  useEffect(() => {
+    window.rerender = rerender;
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>Clocks</title>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <div className="p-8 max-w-7xl mx-auto font-sans">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
